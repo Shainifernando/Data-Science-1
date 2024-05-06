fc5293780d9d2b31c049cae9d09052e3284b2567
Shainifernando <163276569+Shainifernando@users.noreply.github.com>
2024-05-06 23:26:15 +0530

    read the data set app.py

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
+
+Global_superstore_data = pd.read_excel("Global Superstore Lite.xlsx") 
