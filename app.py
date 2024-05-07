b578008dff3646546ea20614185a3e0484b1fd39
Shainifernando <163276569+Shainifernando@users.noreply.github.com>
2024-05-07 11:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
