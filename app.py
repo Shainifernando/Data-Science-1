a96ba06e4d590aa4d109d03147b312e7421e9a8d
Shainifernando <163276569+Shainifernando@users.noreply.github.com>
2024-05-08 22:31:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import openpyxl as pxl
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
