88e203436bd014aed72c4b0d3d093a9dc206a7c7
Shainifernando <163276569+Shainifernando@users.noreply.github.com>
2024-05-08 16:57:41 +0530

    Update app.py - MBA using segments

--- app.py
+++ app.py
@@ -7,12 +7,101 @@ from mlxtend.frequent_patterns import association_rules
 
 #reading the dataset.
 Global_superstore_data = pd.read_excel("Global Superstore Lite.xlsx")
+
 # relationship analysis
 corelation = Global_superstore_data.corr()
 sns.heatmap(corelation, xticklabels=corelation.columns, yticklabels=corelation.columns, annot=True)
 
 # Reading the cleaned dataset
 df = pd.read_excel('cleaned_dataset_global(1).xlsx')
+
+###### MBA using Segments to train the dataset
+## For Segment 1
+
+s1 = (df[df["Segment"] == "Consumer"]
+     .groupby(["Order ID", "Sub-Category"])["Quantity"]
+     .sum().unstack().reset_index().fillna(0)
+     .set_index("Order ID"))
+s1
+
+def encode_units(x):
+    if x <=0:
+        return 0
+    if x >=1:
+        return 1
+
+s1_sets = s1.applymap(encode_units)
+
+frequent_itemsets_s1 = apriori(s1_sets, min_support=0.001, use_colnames = True)
+rules_s1 = association_rules(frequent_itemsets_s1, metric = "lift", min_threshold=1)
+print(rules_s1)
+
+#vis1(heatmap)
+heatmap_data = rules_s1.pivot(index='antecedents', columns='consequents', values='lift')
+plt.figure(figsize=(5, 4))
+sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('lift Heatmap of Association Rules')
+plt.xlabel('Consequents')
+plt.ylabel('Antecedents')
+plt.show()
+
+## For Segment 2
+s2 = (df[df["Segment"] == "Home Office"]
+     .groupby(["Order ID", "Sub-Category"])["Quantity"]
+     .sum().unstack().reset_index().fillna(0)
+     .set_index("Order ID"))
+s2
+
+def encode_units(x):
+    if x <=0:
+        return 0
+    if x >=1:
+        return 1
+
+s2_sets = s2.applymap(encode_units)
+
+frequent_itemsets_s2 = apriori(s2_sets, min_support=0.001, use_colnames = True)
+rules_s2 = association_rules(frequent_itemsets_s2, metric = "lift", min_threshold=1)
+print(rules_s2)
+
+# vis2(heatmap)
+
+heatmap_data = rules_s2.pivot(index='antecedents', columns='consequents', values='lift')
+plt.figure(figsize=(5, 4))
+sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('lift Heatmap of Association Rules')
+plt.xlabel('Consequents')
+plt.ylabel('Antecedents')
+plt.show()
+
+## For Segment 3
+
+s3 = (df[df["Segment"] == "Corporate"]
+     .groupby(["Order ID", "Sub-Category"])["Quantity"]
+     .sum().unstack().reset_index().fillna(0)
+     .set_index("Order ID"))
+s3
+
+def encode_units(x):
+    if x <=0:
+        return 0
+    if x >=1:
+        return 1
+
+s3_sets = s3.applymap(encode_units)
+
+frequent_itemsets_s3 = apriori(s3_sets, min_support=0.001, use_colnames = True)
+rules_s3 = association_rules(frequent_itemsets_s3, metric = "lift", min_threshold=1)
+print(rules_s3)
+
+# vis3(heatmap)
+heatmap_data = rules_s3.pivot(index='antecedents', columns='consequents', values='lift')
+plt.figure(figsize=(10, 8))
+sns.heatmap(heatmap_data, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('lift Heatmap of Association Rules')
+plt.xlabel('Consequents')
+plt.ylabel('Antecedents')
+plt.show()
 
 # MBA findings for each segment
 for segment in Global_superstore_data["Segment"].unique():
